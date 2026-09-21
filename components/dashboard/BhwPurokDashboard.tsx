3b385d422ab56274a356f1b927fbc98a7e113eb7
JOHNLLOYD DONQUE <LLOYDY0510>
2026-09-21 13:21:14 +0800

    updatebhwpurokdash

--- components/dashboard/BhwPurokDashboard.tsx
+++ components/dashboard/BhwPurokDashboard.tsx
@@ -225,8 +225,8 @@ export default async function BhwPurokDashboard() {
           <div className="space-y-2">
             {upcomingCheckups.map((checkup) => {
               const mother = Array.isArray(checkup.pregnant_mothers) 
-                ? checkup.pregnant_mothers[0] 
-                : checkup.pregnant_mothers;
+                ? (checkup.pregnant_mothers as any)[0] 
+                : (checkup.pregnant_mothers as any);
               return (
                 <div
                   key={checkup.id}
